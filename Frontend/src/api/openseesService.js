d68f2b58cef9ff11d836844322f2a4806723844c
Radio-active-Boys <iitjammu7@gmail.com>
2025-07-21 13:23:28 +0530

    Updated

--- Frontend/src/api/openseesService.js
+++ Frontend/src/api/openseesService.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 // Everything under /api will be proxied by Nginx
-// const API_URL = '/api'; // Production
-const API_URL = 'http://127.0.0.1:5000'; // Deveopment
+const API_URL = '/api'; // Production
+// const API_URL = 'http://127.0.0.1:5000'; // Deveopment
 
 export const runAnalysis = async (modelData) => {
   // console.log("Data send to backend", modelData);
